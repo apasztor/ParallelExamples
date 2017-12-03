364bdc5a88c8694548f081d069646d4fd82f0bc8
Attila Pasztor <evilcman@gmail.com>
2017-12-03 14:11:48 +0100

    Changed MPI Sum: data size % num_procs == 0 not assumed anymore

--- MPI/02_basic_sum/check.sh
+++ MPI/02_basic_sum/check.sh
@@ -1,21 +1,13 @@
-size=4000000
-
-echo "-----------------------------------------"
-echo "Generating random sequence               "
-echo "-----------------------------------------"
-echo -e "import random\nfor i in range(${size}): print(random.uniform(0,1))" | python3 > input.dat
-
-echo "-----------------------------------------"
-echo "Calculating sums with awk:"
-echo "-----------------------------------------"
-
-cat input.dat | awk 'BEGIN{x=0}{x+=$1}     END{printf("Sum        : %f\n", x)}'
-cat input.dat | awk 'BEGIN{x=0}{x+=$1*$1}  END{printf("Square sum : %f\n", x)}'
-cat input.dat | awk 'BEGIN{x=0}{x+=sin($1)}END{printf("Sine sum   : %f\n", x)}'
-
-for nproc in 1 2 4 8; do
-    echo "-----------------------------------------"
-    echo "Sum with our MPI code on $nproc process"
-    echo "-----------------------------------------"
-    mpirun -np $nproc ./sum input.dat ${size}
+for size in 201 4321 16000 30001; do
+    echo "------------------------------ global size: $size -------------------------------------------"
+    echo -e "import random\nfor i in range(${size}): print(random.uniform(0,1))" | python3 > input.dat
+    
+    cat input.dat | awk 'BEGIN{x=0;y=0;z=0;}\
+                         {x+=$1;y+=$1*$1;z+=sin($1)}\
+                         END{printf("Sum by awk       |                           |   ");\
+                         printf("Sum        : %f   |   Square sum : %f   |   Sine sum   : %f\n", x,y,z)}'
+    
+    for nproc in 1 2 4 8; do
+        mpirun -np $nproc ./sum input.dat ${size}
+    done
 done
--- MPI/02_basic_sum/sum.c
+++ MPI/02_basic_sum/sum.c
@@ -3,14 +3,29 @@
 #include <math.h>
 #include <mpi.h>
 
-void read_array(double ** local_x, int local_n, int global_n, int rank, int num_procs, char input_filename[]){
-    *local_x=(double *)malloc(local_n*sizeof(double));
+int get_local_n(int global_n, int rank, int num_procs){
+    /* Calculates local size of the array to store in memory */
+    int local_n;
+    if(global_n % num_procs == 0){
+        local_n = global_n / num_procs;
+    }else{
+        if(rank != num_procs-1) {local_n = (global_n / num_procs) + 1;             } 
+        else                    {local_n = global_n % ((global_n / num_procs) + 1);}
+    } 
+    return local_n;
+}
+
+void read_array(double ** local_x, int global_n, int rank, int num_procs, char input_filename[]){
+    /* Uses process num_procs-1 to read in the input from a file and distribute it to the processes */
+    int local_n_max = get_local_n(global_n,0,num_procs);
+    *local_x=(double *)malloc(local_n_max*sizeof(double));
     MPI_Status * status;
 
     if(rank == num_procs-1){
-        printf ("Reading data with proc. rank %d and distributing data to other procs\n",rank);
+        // printf ("Reading data with proc. rank %d and distributing data to other procs\n",rank);
         FILE *infile=fopen(input_filename,"r");
         for(int iproc=0; iproc<num_procs; iproc++){
+            int local_n = get_local_n(global_n,iproc,num_procs);
             for(int iloc=0; iloc<local_n; iloc++){
                 double temp;
                 fscanf(infile, "%lf\n", &temp);
@@ -22,79 +37,86 @@ void read_array(double ** local_x, int local_n, int global_n, int rank, int num_
             }
         }
         fclose(infile);
-        printf("Data readin complete\n");
     }else{
+        int local_n = get_local_n(global_n,rank,num_procs);
         int tag=rank;
         MPI_Recv((*local_x),local_n,MPI_DOUBLE,num_procs-1,tag,MPI_COMM_WORLD,status);
     }
 }
 
-double serial_sum(double x[], int n){
-    double sum=0.0;
-    for(int i=0; i<n; i++){
-        sum += x[i];
+double serial_sum(double loc_x[], int loc_n){
+    /* serial sum running on one process */
+    double loc_sum=0.0;
+    for(int i=0; i<loc_n; i++){
+        loc_sum += loc_x[i];
     }
-    return sum;
+    return loc_sum;
 }
 
-double serial_funsum(double x[], int n, double (*f)(double)){
-    double sum=0.0;
-    for(int i=0; i<n; i++)
-        sum += f(x[i]);
-    return sum;
+double serial_funsum(double loc_x[], int loc_n, double (*f)(double)){
+    /* serial sum running on one process - generic version using a function pointer */
+    double loc_sum=0.0;
+    for(int i=0; i<loc_n; i++)
+        loc_sum += f(loc_x[i]);
+    return loc_sum;
 }
 
-double parallel_sum(double local_x[], int local_n){
+double parallel_sum(double local_x[], int global_n, int rank, int num_procs){
+    /* parallel sum for the global array */
+    int local_n = get_local_n(global_n, rank, num_procs);
     double local_sum = serial_sum(local_x, local_n);
     double global_sum = 0.0;
-    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, num_procs-1, MPI_COMM_WORLD);
     return global_sum;
 }
 
-double parallel_funsum(double local_x[], int local_n, double (*f)(double)){
+double parallel_funsum(double local_x[], int global_n, int rank, int num_procs, double (*f)(double)){
+    /* parallel sum for the global array - generic version with a function pointer */
+    int local_n = get_local_n(global_n, rank, num_procs);
     double local_sum = serial_funsum(local_x, local_n, f);
     double global_sum = 0.0;
-    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, num_procs-1, MPI_COMM_WORLD);
     return global_sum;
 }
 
 double sq(double x){return x*x;}
 
 int main(int argc, char * argv[]){
-    int num_procs,my_rank;
+    int num_procs,rank;
     MPI_Init     (&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if(argc != 3){
         printf("usage: %s inputfile global_n\n",argv[0]);
         MPI_Abort(MPI_COMM_WORLD,1);
     }
-    
+   
     int global_n=atoi(argv[2]);
-    if(global_n % num_procs != 0){
-        printf("ERROR: global_n mod num_procs != 0\n");
-        MPI_Abort(MPI_COMM_WORLD,1);
-    }
+    //if(rank == num_procs-1){
+    //    printf("Local size in the processes:");
+    //    for(int i=0; i<num_procs; i++){
+    //        printf(" %d ",get_local_n(global_n,i,num_procs));
+    //    }
+    //    printf("\n");
+    //}
 
-    int local_n=global_n/num_procs;
     MPI_Barrier(MPI_COMM_WORLD);
     
     double * local_x;
-    read_array(&local_x,local_n,global_n,my_rank,num_procs,argv[1]);
+    read_array(&local_x,global_n,rank,num_procs,argv[1]);
 
     double t1;
     t1 = MPI_Wtime(); 
-    double sum  = parallel_sum(local_x,local_n);
-    double sum2 = parallel_funsum(local_x,local_n,&sq);
-    double sum3 = parallel_funsum(local_x,local_n,&sin);
-    if(my_rank == num_procs-1){
-        printf("Calculation took %f second\n", MPI_Wtime()-t1); 
-    }
+    double sum  = parallel_sum   (local_x,global_n,rank,num_procs);
+    double sum2 = parallel_funsum(local_x,global_n,rank,num_procs,&sq);
+    double sum3 = parallel_funsum(local_x,global_n,rank,num_procs,&sin);
     
-    if(my_rank==0){
-        printf("Sum        : %f\n",sum);
-        printf("Square sum : %f\n",sum2);
+    if(rank==num_procs-1){
+        printf("Num_procs  : %d   |   ",num_procs);
+        printf("Calc_time  : %f   |   ",MPI_Wtime()-t1);
+        printf("Sum        : %f   |   ",sum);
+        printf("Square sum : %f   |   ",sum2);
         printf("Sine sum   : %f\n",sum3);
     }
 
